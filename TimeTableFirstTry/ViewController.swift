9e008a04a7b7e638c9a416a99b4d1737fb94e075
Aurel <aurel.feer@gmail.com>
2015-10-25 19:27:09 +0100

    Initial Commit

---
+++ TimeTableFirstTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTableFirstTry
+//
+//  Created by Aurel Feer on 25.10.15.
+//  Copyright © 2015 Aurel Feer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
