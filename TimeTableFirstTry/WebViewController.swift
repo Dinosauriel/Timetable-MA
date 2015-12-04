02375ace933f645fb52d669863fd408b48012303
Aurel <aurel.feer@gmail.com>
2015-12-04 16:45:33 +0100

    App now automatically switches to Timetable once token URL is loaded

--- TimeTableFirstTry/WebViewController.swift
+++ TimeTableFirstTry/WebViewController.swift
@@ -8,23 +8,34 @@
 
 import UIKit
 
-class WebView: UIViewController, UIWebViewDelegate {
+class WebViewController: UIViewController, UIWebViewDelegate {
     
     //OUTLETS
     @IBOutlet weak var webView: UIWebView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    //IDENTIFIERS
+    let mainAppCycleSegueIdentifier = "showMainAppCycle"
     
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        print(request)
-        print("Hi")
+        
+        let URLString = request.URL?.absoluteString
+        
+        if URLString!.containsString("uniapp://klw-stupla-app#access_token=") {
+            print("Token URL Loaded!")
+            self.performSegueWithIdentifier(mainAppCycleSegueIdentifier, sender: self)
+        }
+            
         return true
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.delegate = self
+    }
+    
     override func viewWillAppear(animated: Bool) {
+
         let URLforRequest = NSURL(string: "https://oauth.tam.ch/signin/klw-stupla-app?response_type=token&client_id=0Wv69s7vyidj3cKzNckhiSulA5on8uFM&redirect_uri=uniapp%3A%2F%2Fklw-stupla-app&_blank&scope=all")
         let request = NSURLRequest(URL: URLforRequest!)
         
