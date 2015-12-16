747dd9cef7714819cb48b6af6e4f7335c4b9e249
Helibum1 <lukas.boner@stud.ksimlee.ch>
2015-12-16 18:59:11 +0100

    pulling

--- TimeTableFirstTry/AppDelegate.swift
+++ TimeTableFirstTry/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var UserDefaults = NSUserDefaults.standardUserDefaults()
     let collectionView = TTCollectionViewController()
     
+    func getView() -> TTCollectionViewController {
+        return collectionView
+    }
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         // DETECTING FIRST LAUNCH
