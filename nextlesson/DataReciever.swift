0dbb3fda39cc68e13a35c61d9a9d772de2771bc7
Helibum1 <lukas.boner@stud.ksimlee.ch>
2016-01-15 18:11:04 +0100

    Added data-class

--- nextlesson/DataReciever.swift
+++ nextlesson/DataReciever.swift
@@ -9,7 +9,43 @@
 import Foundation
 
 class DataReciever {
+    
+    let sharedDefaults = NSUserDefaults(suiteName: "group.lee.labf.timetable")
+    let urlBuilder:URLBuildingSupport = URLBuildingSupport()
     func getEntireTable() {
-        //Please add entire Table
+        let token:String = (sharedDefaults?.objectForKey("token"))! as! String
+        
+        let URLRequestString = urlBuilder.getURLForCurrentDate(token)
+        
+        let requestURL = NSURL(string: URLRequestString)
+        let request = NSMutableURLRequest(URL: requestURL!)
+        request.HTTPMethod = "GET"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
+            do {
+                if let s:NSString = String(data: data!, encoding: NSUTF8StringEncoding) {
+                    var cleaned:String = s as String
+                    cleaned = s.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "()"))
+                    let cleanedData = (cleaned as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+                    let dataDict:NSMutableDictionary = try NSJSONSerialization.JSONObjectWithData(cleanedData!, options: NSJSONReadingOptions.MutableContainers) as! NSMutableDictionary
+                    
+                    let keys = dataDict.allKeys
+                    if !keys.contains({$0 as! String == "code"}) {
+                        //success -> call draw function
+                    } else {
+                        //not successful
+                    }
+                    
+                }
+            } catch let myJSONError {
+                //In the case that the parsing of the data fails print the error
+                //(Never happened until now)
+                print(myJSONError)
+            }
+        })
+        task.resume()
     }
+    
 }
